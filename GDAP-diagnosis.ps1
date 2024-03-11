1739cac5fa40538583d3450e65a4d6e0e242e38e
Roel van der Wegen <github@aeternus.tech>
2024-03-11 11:53:10 +0100

    Create GDAP-diagnosis.ps1

---
+++ GDAP-diagnosis.ps1
@@ -0,0 +1,12 @@
+
+# Check existence AdminAgents
+  # Check membership of AdminAgents
+
+# Check relationship present on tenant
+  # Check relationship roles
+  # Check relationship mapped groups
+  # Check membership of mapped groups
+
+# Remediate?
+
+# GA Needed?
